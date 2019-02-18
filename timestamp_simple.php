16f14b5a2613dd4ab0e0a290bd398bf2371d79d2
chainaccelerator <39132119+chainaccelerator@users.noreply.github.com>
2019-02-18 08:38:54 +0100

    Add files via upload

--- timestamp_simple.php
+++ timestamp_simple.php
@@ -1,1 +1,25 @@
+<?php
 
+trait timestamp_simple {
+
+    private static $timestamp_timezone = 'UTC';
+
+    public static function timestamp_init() {
+        
+        date_default_timezone_set($timestamp_timezone);
+    }
+
+    public static function timestamp_get(){
+
+        return time();
+    }
+
+    public function timestamp_data(string $data){
+
+        $data .= self::$timestamp_sep.$this->date;
+        $this->hash = hash(self::$hash_algo, $data.$this->date);
+        $data .= self::$hash_sep.$this->hash;
+
+        return $data;
+    }
+}
