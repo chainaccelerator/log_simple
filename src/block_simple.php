7a99517d8ad7bf06d51f359e0ba72ca4a0a0eb56
chainaccelerator <nicolas.cantu@chainaccelerator.com>
2019-03-08 10:07:00 +0100

    init

--- src/block_simple.php
+++ src/block_simple.php
@@ -22,19 +22,5 @@ trait Block_simple {
         return true;
     }
 
-    /**
-     * @var string $step_name
-     * @var string $height_new_push_address
-     * @return Block_simple_data
-     */
-    public function block_get(string $step_name, string $height_new_push_address) {
-
-        $response = $this->socket_client_push_broadcast_request_count('/'.$step_name.'/block/'.$height_new_push_address);
-
-        $block_data = $response->data->block_data;
-
-        return $block_data;
-    }
-
 
 }
