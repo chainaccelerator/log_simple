0455c2fec27ffdcd4f908b9dc49ae68c32c43e06
chainaccelerator <nicolas.cantu@chainaccelerator.com>
2019-03-08 11:04:13 +0100

    init

--- src/log_simple_code_interface.php
+++ src/log_simple_code_interface.php
@@ -15,10 +15,24 @@ class Log_simple_code_Interface
         $this->public_key = $public_key;
     }
 
-    public function function_add(Log_simple_code_function $log_function_argument)
+    public function function_add(Log_simple_code_function $log_function)
     {
 
-        $this->function_list[] = $log_function_argument;
+        $this->function_list[] = $log_function;
+
+        return count($this->function_list);
+    }
+
+    public function function_get(Log_simple_code_function $function_index)
+    {
+
+        return $this->function_list[$function_index];
+    }
+
+    public function function_set(Log_simple_code_function $function_index, Log_simple_code_function $log_function)
+    {
+
+        $this->function_list[$function_index] = $log_function;
 
         return true;
     }
--- src/log_simple_storage_signed.php
+++ src/log_simple_storage_signed.php
@@ -1,26 +1,44 @@
 <?php
 
 
+/**
+ * Class Log_simple_storage_signed
+ */
 class Log_simple_storage_signed
 {
 
     use Sign_simple;
 
+    /**
+     * @var int
+     */
     private static $log_storage_public_key_hash_algo = OPENSSL_ALGO_SHA1;
 
+    /**
+     * @var string
+     */
     public $public_key_hashed;
+    /**
+     * @var int
+     */
     public $right_level = 1;
+    /**
+     * @var bool
+     */
     public $sign;
 
+    /**
+     * Log_simple_storage_signed constructor.
+     * @param string $data_to_store
+     * @param string $public_key
+     * @param int $right_level
+     */
     public function __construct(string $data_to_store, string $public_key, int $right_level)
     {
-
-        $this->sign_init();
 
         $this->public_key_hashed = hash(self::$log_storage_public_key_hash_algo, $public_key);
         $this->right_level = $right_level;
         $this->sign = $this->sign($data_to_store . $this->right_level);
     }
-
 }
 
